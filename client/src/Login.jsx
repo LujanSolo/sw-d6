d2ec0d20894f11b9e29061a759ed469b2b08e529
LujanSolo <lujansolo@proton.me>
2023-10-02 17:47:09 -0700

    updated app.jsx with basic login form

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function LoginForm() {
@@ -14,7 +14,7 @@ function LoginForm() {
     <div className="container">
       <h2>LOGIN FORM</h2>
       <Form onSubmit={handleLoginSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Group controlId="formBasicUsername">
           <Form.Label>Login name</Form.Label>
           <Form.Control
             type="text"
@@ -22,12 +22,9 @@ function LoginForm() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
