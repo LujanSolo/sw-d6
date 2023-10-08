ae95e8a79d2e9c0bd4672cc533e97b43dd1e3ff5
LujanSolo <lujansolo@proton.me>
2023-10-08 10:53:27 -0700

    in config.js, changed Sequelize const for testing purposes

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,28 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+
+const sequelize = new Sequelize(
+
+  //database name
+  'weapons_db',
+  //user
+  'root',
+  //password
+  'myPassword',
+  {
+    //database location
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-  });
+  }
+);
 
 module.exports = sequelize;
