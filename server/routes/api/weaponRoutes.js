966e9fc5f6adad051d4bda92a85a1affeb6c4bf9
LujanSolo <lujansolo@proton.me>
2023-10-08 20:38:40 -0700

    added bulkCreate values for starter weapons

--- server/routes/api/weaponRoutes.js
+++ server/routes/api/weaponRoutes.js
@@ -4,7 +4,7 @@ const Weapon = require('../../models/Weapon');
 
 router.post('/', (req, res) => {
   Weapon.create({
-    title: req.body.title,
+    name: req.body.title,
     damage: req.body.damage
   })
     .then((newWeapon) => {
@@ -14,5 +14,46 @@ router.post('/', (req, res) => {
       res.json(err);
     }); 
 });
+
+router.post('/seed', (req, res) => {
+  Weapon.bulkCreate([
+    {
+      name: "Longbow",
+      damage: 2,
+      bonus_damage: 2
+    },
+    {
+      name: "Black-powder Pistol",
+      damage: 2,
+      bonus_damage: 2
+    },
+    {
+      name: "Musket",
+      damage: 3,
+      bonus_damage: 0
+    },
+    {
+      name: "Blaster Pistol",
+      damage: 4,
+      bonus_damage: 0
+    },
+    {
+      name: "Heavy Blaster Pistol",
+      damage: 5,
+      bonus_damage: 0
+    },
+    {
+      name: "Blaster Rifle",
+      damage: 5,
+      bonus_damage: 0
+    },
+    {
+      name: "Heavy Blaster Rifle",
+      damage: 5,
+      bonus_damage: 1
+    },
+
+  ])
+})
 
 module.exports = router;
